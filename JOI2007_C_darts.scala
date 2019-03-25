c214a8af120a4820b1c8362224265dced5d7c4a9
Tatsuki Saito <cc_called_pizza@yahoo.co.jp>
2019-03-26 03:00:58 +0900

    update answers of JOI 2007

--- JOI2007_C_darts.scala
+++ JOI2007_C_darts.scala
@@ -1,7 +1,35 @@
 object Main {
   def main(args: Array[String]) = {
-    val lines = Source.stdin.getLines.toStream
-    val Array(height, width) = lines.head.split(" ").map(_.toInt)
+    val lines = scala.io.Source.stdin.getLines.toStream
+    val Array(numLines, limit) = lines.head.split(" ").map(_.toInt)
+    val scores = 0 +: lines.drop(1).map(_.toInt).toArray
+    val doubles = Array.fill((numLines+1)*(numLines+1))(0)
 
+    var max = 0
+    for {
+      throw1 <- scores
+      throw2 <- scores
+    } {
+      doubles(i) = throw1 + throw2
+      i += 1
+    }
+    java.util.Arrays.sort(doubles)
+
+    for (double <- doubles) {
+      var cand = double
+      if (cand <= limit) {
+        if (cand < limit) cand = cand + bin(0, doubles.length, limit - cand, doubles)
+        if (cand > max) max = cand
+      }
+    }
+
+    println(max)
+  }
+
+  def bin(from: Int, to: Int, limit: Int, array: Array[Int]): Int = {
+    val half = (from + to) / 2
+    if (from == half) return array(from)
+    val cand = array(half)
+    if (cand <= limit) bin(half, to, limit, array) else bin(from, half, limit, array)
   }
 }
