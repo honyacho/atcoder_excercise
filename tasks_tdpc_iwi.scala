4e3cd700cd5ad7c0b9b8c5ad78f607e8dab40986
Tatsuki Saito <cc_called_pizza@yahoo.co.jp>
2019-04-05 15:03:09 +0900

    update .gitignore

---
+++ tasks_tdpc_iwi.scala
@@ -0,0 +1,32 @@
+import scala.io.Source
+import scala.collection.mutable.Map
+
+object Main {
+  val memo = Map[String, Int]()
+
+  def main(args: Array[String]) = {
+    val lines = Source.stdin.getLines.toStream 
+
+    println(dp(lines.head))
+  }
+
+  def dp(input: String): Int = {
+    val resOpt = memo.get(input)
+    if (resOpt.nonEmpty) {
+      resOpt.get
+    } else {
+      var max = 0
+
+      if (input.length >= 3) {
+        (0 to input.length - 3).foreach { i =>
+          if (input(i) == 'i' && input(i+1) == 'w' && input(i+2) == 'i')
+            val next = dp(input.slice(0, i) + input.slice(i+3, input.length)) + 1
+            if (next > max) max = next
+          }
+        }
+      }
+
+      max
+    }
+  }
+}
