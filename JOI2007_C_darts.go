d022ba11fc9066bdd06692a520508df106565ad4
Tatsuki Saito <cc_called_pizza@yahoo.co.jp>
2019-04-06 04:52:38 +0900

    format

--- JOI2007_C_darts.go
+++ JOI2007_C_darts.go
@@ -1,48 +1,51 @@
 package main
+
 import "fmt"
 import "sort"
 
 func binSearch(from int, to int, limit int, inputs []int) int {
-  half := (from + to) / 2
-  if (from == half) {
-    return inputs[from]
-  }
+	half := (from + to) / 2
+	if from == half {
+		return inputs[from]
+	}
 
-  if (inputs[half] <= limit) {
-    return binSearch(half, to, limit, inputs)
-  } else {
-    return binSearch(from, half, limit, inputs)
-  }
+	if inputs[half] <= limit {
+		return binSearch(half, to, limit, inputs)
+	} else {
+		return binSearch(from, half, limit, inputs)
+	}
 }
 
 func main() {
-  var length int
-  var limit int
-  var max int = 0
-  fmt.Scan(&length)
-  length = length + 1
-  fmt.Scan(&limit)
-  inputs := make([]int, length)
-  doubles := []int{}
+	var length int
+	var limit int
+	var max int = 0
+	fmt.Scan(&length)
+	length = length + 1
+	fmt.Scan(&limit)
+	inputs := make([]int, length)
+	doubles := []int{}
 
-  inputs[0] = 0
-  for i := 1; i < length; i++ { fmt.Scan(&inputs[i]) }
-  for _, num1 := range inputs {
-    for _, num2 := range inputs {
-      doubles = append(doubles, num1 + num2)
-    }
-  }
-  sort.Ints(doubles)
+	inputs[0] = 0
+	for i := 1; i < length; i++ {
+		fmt.Scan(&inputs[i])
+	}
+	for _, num1 := range inputs {
+		for _, num2 := range inputs {
+			doubles = append(doubles, num1+num2)
+		}
+	}
+	sort.Ints(doubles)
 
-  for _, value := range doubles {
-    if value <= limit {
-      if value < limit {
-        value = value + binSearch(0, length * length, limit - value, doubles)
-      }
-      if max < value {
-        max = value
-      }
-    }
-  }
-  fmt.Println(max)
+	for _, value := range doubles {
+		if value <= limit {
+			if value < limit {
+				value = value + binSearch(0, length*length, limit-value, doubles)
+			}
+			if max < value {
+				max = value
+			}
+		}
+	}
+	fmt.Println(max)
 }
--- abc012_4_バスと避けられない運命.go
+++ abc012_4_バスと避けられない運命.go
@@ -1,59 +1,60 @@
 package main
+
 import "fmt"
 
 func main() {
-  var numStops int
-  var numLines int
-  fmt.Scan(&numStops)
-  fmt.Scan(&numLines)
+	var numStops int
+	var numLines int
+	fmt.Scan(&numStops)
+	fmt.Scan(&numLines)
 
-  memo := make([][]int, numStops)
+	memo := make([][]int, numStops)
 
-  for i := 0; i < numStops; i++ {
-    memo[i] = make([]int, numStops)
-    for j := 0; j < numStops; j++ {
-      memo[i][j] = 2147483647
-    }
-  }
+	for i := 0; i < numStops; i++ {
+		memo[i] = make([]int, numStops)
+		for j := 0; j < numStops; j++ {
+			memo[i][j] = 2147483647
+		}
+	}
 
-  for i := 0; i < numLines; i++ {
-    var left int
-    var right int
-    fmt.Scan(&left)
-    fmt.Scan(&right)
-    fmt.Scan(&memo[left-1][right-1])
-    memo[right-1][left-1] = memo[left-1][right-1]
-  }
+	for i := 0; i < numLines; i++ {
+		var left int
+		var right int
+		fmt.Scan(&left)
+		fmt.Scan(&right)
+		fmt.Scan(&memo[left-1][right-1])
+		memo[right-1][left-1] = memo[left-1][right-1]
+	}
 
-  for i := 0; i < numStops; i++ {
-    for j := 0; j < numStops; j++ {
-      for k := 0; k < numStops; k++ {
-        if j == k {
-          memo[j][k] = 0
-        } else {
-          if memo[j][k] < memo[j][i] + memo[i][k] {
-            memo[j][k] = memo[j][k]
-          } else {
-            memo[j][k] = memo[j][i] + memo[i][k]
-          }
-        }
-      }
-    }
-  }
+	for i := 0; i < numStops; i++ {
+		for j := 0; j < numStops; j++ {
+			for k := 0; k < numStops; k++ {
+				if j == k {
+					memo[j][k] = 0
+				} else {
+					if memo[j][k] < memo[j][i]+memo[i][k] {
+						memo[j][k] = memo[j][k]
+					} else {
+						memo[j][k] = memo[j][i] + memo[i][k]
+					}
+				}
+			}
+		}
+	}
 
-  var min int = 2147483647
-  for i := 0; i < numStops; i++ {
-    var max int = 0
-    for j := 0; j < numStops; j++ {
-      if memo[i][j] > max {
-        max = memo[i][j]
-      }
-    }
+	var min int = 2147483647
+	for i := 0; i < numStops; i++ {
+		var max int = 0
+		for j := 0; j < numStops; j++ {
+			if memo[i][j] > max {
+				max = memo[i][j]
+			}
+		}
 
-    if max < min {
-      min = max
-    }
-  }
+		if max < min {
+			min = max
+		}
+	}
 
-  fmt.Println(min)
+	fmt.Println(min)
 }
