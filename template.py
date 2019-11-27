a480d7796c0f8bdfff6c46be07283b1e5b565b52
Tatsuki Saito <cc_called_pizza@yahoo.co.jp>
2019-11-28 01:35:48 +0900

    update template

--- template.py
+++ template.py
@@ -1,12 +1,17 @@
+import math
 import sys
 sys.setrecursionlimit(10**7)
 INTMAX = 9223372036854775807
 INTMIN = -9223372036854775808
-MOD = 1000000007
-def POW(x, y): return pow(x, y, MOD)
-def INV(x, m=MOD): return pow(x, m - 2, m)
-def DIV(x, y, m=MOD): return (x * INV(y, m)) % m
+DVSR = 1000000007
+def POW(x, y): return pow(x, y, DVSR)
+def INV(x, m=DVSR): return pow(x, m - 2, m)
+def DIV(x, y, m=DVSR): return (x * INV(y, m)) % m
 def LI(): return [int(x) for x in input().split()]
 def LF(): return [float(x) for x in input().split()]
 def LS(): return input().split()
 def II(): return int(input())
+def FLIST(n):
+    res = [1]
+    for i in range(1, n+1): res.append(res[i-1]*i%DVSR)
+    return res
