fa32a0d02a71dab8335fbf572dc80fc2686b64d1
Tatsuki Saito <cc_called_pizza@yahoo.co.jp>
2020-01-26 23:01:43 +0900

    update

--- abc150/e.cpp
+++ abc150/e.cpp
@@ -10,12 +10,26 @@ template<typename T> void show(const vector<T>& v){ for(const auto& e : v){ cout
 template<typename T> void show(const vector<vector<T>>& vv){ for(const auto& v : vv){ show(v); } }
 template<class Iter> void show(Iter first, Iter last){ for(auto it=first; it != last; it++){ cout << *it << " "; }; cout << endl; }
 const ll DVSR = 10e9+7;
+template <typename T> T modpow(T base, T exp, T modulus) {
+  base %= modulus; T result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = (result * base) % modulus;
+    base = (base * base) % modulus;
+    exp >>= 1;
+  }
+  return result;
+}
 
 int main(int argc, char const *argv[])
 {
   ll N; cin >> N;
   vector<ll> CS(N);
   REP(i, N) cin >> CS[i];
-  show("hello, world");
+  vector<ll> facts(N+1), CUM(N+1);
+  facts[0] = 1;
+  CUM[0] = 0;
+  RNG(i,1,N+1) facts[i] = facts[i-1]*i % DVSR;
+  sort(CS.begin(), CS.end());
+  REP(i, N+1) {}
   return 0;
 }
