042a060d935c3b783ab90374abdc6fc64a95a26b
Tatsuki Saito <cc_called_pizza@yahoo.co.jp>
2020-02-21 17:56:37 +0900

    add template

--- template.py
+++ template.py
@@ -8,27 +8,11 @@ DVSR = 1000000007
 def POW(x, y): return pow(x, y, DVSR)
 def INV(x, m=DVSR): return pow(x, m - 2, m)
 def DIV(x, y, m=DVSR): return (x * INV(y, m)) % m
-def LI(): return [int(x) for x in input().split()]
-def LF(): return [float(x) for x in input().split()]
-def LS(): return input().split()
-def II(): return int(input())
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LF(): return [float(x) for x in sys.stdin.readline().split()]
+def LS(): return sys.stdin.readline().split()
+def II(): return int(sys.stdin.readline())
 def FLIST(n):
     res = [1]
     for i in range(1, n+1): res.append(res[i-1]*i%DVSR)
     return res
-def gcd(x, y):
-    if x < y: x, y = y, x
-    div = x % y
-    while div != 0:
-        x, y = y, div
-        div = x % y
-    return y
-def primes(n):
-    P=[2]
-    for L in range(3, n):
-        isP = True
-        for L2 in P:
-            isP = isP and L%L2
-            if not isP: break
-        if isP: P.append(L)
-    return P
