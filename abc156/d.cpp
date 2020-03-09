b1caee6e09def5eb446972bb7a8e15f925aa7782
honyacho <cc_called_pizza@yahoo.co.jp>
2020-03-10 01:42:57 +0900

    update

--- abc156/d.cpp
+++ abc156/d.cpp
@@ -16,8 +16,22 @@ template<typename T> T mod_pow(T x, T n, const T &p) { T ret = 1; while(n > 0) {
 template<typename T> T mod_inv(T x, const T &p) { return mod_pow(x, p-2, p); }
 const ll DVSR = 1e9+7;
 
-
 int main(int argc, char const *argv[])
 {
+  ll N, A, B; cin >> N >> A >> B;
+  ll patA = 1, patB = 1, res = mod_pow(2LL, N, DVSR)-1;
+  RNG(i, 1, A+1) patA = (patA*i)%DVSR;
+  patA = mod_inv(patA, DVSR);
+  REP(i, A) patA = (patA*(N-i))%DVSR;
+  res -= patA;
+  res %= DVSR;
+
+  RNG(i, 1, B+1) patB = (patB*i)%DVSR;
+  patB = mod_inv(patB, DVSR);
+  REP(i, B) patB = (patB*(N-i))%DVSR;
+  res -= patB;
+  res %= DVSR;
+
+  cout << (res + DVSR)%DVSR << endl;
   return 0;
 }
