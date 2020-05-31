58aade506c6f1aad4f8c012c126776867338134a
honyacho <igatih@gmail.com>
2020-06-01 00:35:16 +0900

    Update

---
+++ abc169/b.py
@@ -0,0 +1,33 @@
+import sys
+import math
+import heapq
+sys.setrecursionlimit(10**7)
+INTMAX = 9223372036854775807
+INTMIN = -9223372036854775808
+DVSR = 1000000007
+def POW(x, y): return pow(x, y, DVSR)
+def INV(x, m=DVSR): return pow(x, m - 2, m)
+def DIV(x, y, m=DVSR): return (x * INV(y, m)) % m
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LF(): return [float(x) for x in sys.stdin.readline().split()]
+def LS(): return sys.stdin.readline().split()
+def II(): return int(sys.stdin.readline())
+def FLIST(n):
+    res = [1]
+    for i in range(1, n+1): res.append(res[i-1]*i%DVSR)
+    return res
+
+N=II()
+LIS=LI()
+
+MX=1000000000000000000
+res = 1
+if 0 in LIS:
+    print(0)
+else:
+    for i in LIS:
+        res *= i
+        if res > MX:
+            print(-1)
+            exit(0)
+    print(res)
