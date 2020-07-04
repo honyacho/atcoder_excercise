761a2ebb2bf2eac66f281df3b53ed1730ff92e70
honyacho <igatih@gmail.com>
2020-07-05 01:39:41 +0900

    mv

--- abc172/d.js
+++ abc172/d.js
@@ -1,7 +1,7 @@
-var fs = require("fs");
-var line = fs.readFileSync("/dev/stdin");
-var N = Number(line);
-var res = 0;
+const fs = require("fs");
+const line = fs.readFileSync("/dev/stdin");
+const N = Number(line);
+let res = 0;
 
 for (let i = 1; i <= N; i++) {
   const n = Math.floor(N / i);
