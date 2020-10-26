f0b0c279aae4b2cdba98f1063dfa84d5b06a48ff
Tatsuki Saito <cc_called_pizza@yahoo.co.jp>
2020-10-26 16:27:11 +0900

    Update

---
+++ abc180/c.py
@@ -0,0 +1,33 @@
+import sys
+import math
+import heapq
+sys.setrecursionlimit(10**7)
+INTMAX = 9223372036854775807
+INTMIN = -9223372036854775808
+DVSR = 1000000007
+def POW(x, y): return pow(x, y, DVSR)
+def INV(x, m=DVSR): return pow(x, m - 2, m)
+def DIV(x, y, m=DVSR): return (x * INV(y, m)) % m
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LF(): return [float(x) for x in sys.stdin.readline().split()]
+def LS(): return sys.stdin.readline().split()
+def II(): return int(sys.stdin.readline())
+def FLIST(n):
+    res = [1]
+    for i in range(1, n+1): res.append(res[i-1]*i%DVSR)
+    return res
+
+
+def main():
+    N=II()
+    res=set()
+    for i in range(1, int(math.sqrt(float(N)))+1):
+        if N%i == 0:
+            res.add(i)
+            res.add(N//i)
+    for i in sorted(list(res)):
+        print(i)
+
+
+if __name__ == '__main__':
+    main()
