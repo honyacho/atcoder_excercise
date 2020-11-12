d7432b2b4bf6dee4e9616ce6ccbdb7a70946bf37
honyacho <igatih@gmail.com>
2020-11-13 08:05:48 +0900

    update

--- template.py
+++ template.py
@@ -8,8 +8,8 @@ DVSR = 1000000007
 def POW(x, y): return pow(x, y, DVSR)
 def INV(x, m=DVSR): return pow(x, m - 2, m)
 def DIV(x, y, m=DVSR): return (x * INV(y, m)) % m
-def LI(): return [int(x) for x in sys.stdin.readline().split()]
-def LF(): return [float(x) for x in sys.stdin.readline().split()]
+def LI(): return map(int, sys.stdin.readline().split())
+def LF(): return map(float, sys.stdin.readline().split())
 def LS(): return sys.stdin.readline().split()
 def II(): return int(sys.stdin.readline())
 def FLIST(n):
